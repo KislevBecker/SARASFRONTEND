548f73f21207d1a769348df88c5d05ff6e521562
KislevBecker <kisbecker@gmail.com>
2022-10-27 09:30:36 -0600

    second trial

---
+++ SARASWEB.Frontend.Transversal/DTO/PuntajeDTO/PuntajeFrmDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SARASWEB.Frontend.Transversal.DTO.PuntajeDTO
+{
+    public class PuntajeFrmDTO
+    {
+        public int? Id { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar una descripcion para el puntaje")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar la categoria de riesgo")]
+        [MaxLength(200, ErrorMessage = "La longitud máxima es de 3 caracteres")]
+        public int IdCategoria { get; set; }
+    }
+}
